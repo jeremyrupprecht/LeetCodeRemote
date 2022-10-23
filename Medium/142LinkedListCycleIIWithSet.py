641e999a16d9100e734cddeef7a099ad68b9dfbd
Jeremy <jdavid.rupprecht@gmail.com>
2022-10-22 20:14:45 -0600

    Adding all medium tier questions done so far

---
+++ Medium/142LinkedListCycleIIWithSet.py
@@ -0,0 +1,70 @@
+# Definition for singly-linked list.
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        set1 = set()
+        
+        current = head
+        
+        while current:
+            
+            if not current in set1:
+                set1.add(current)
+                current = current.next
+            else:
+                return current
+
+        return None
+
+    def test(self, head):
+        while head:
+            print(head.val)
+            head = head.next
+
+if __name__ == "__main__":
+    
+    list = []
+    one = ListNode(1)
+    list.append(one)
+    
+    two = ListNode(2)
+    list.append(two)
+    one.next = two
+    
+    three = ListNode(3)
+    list.append(three)    
+    two.next = three
+    
+    four = ListNode(4)
+    list.append(four)
+    three.next = four
+    
+    five = ListNode(5)
+    list.append(five)
+    four.next = five
+
+    # this node cycles back to node 4
+
+    six = ListNode(6)
+    list.append(six)
+    five.next = six
+    six.next = four
+
+
+    s1 = Solution()
+    node = s1.detectCycle(one)
+    print(node.val)
+
+#    while node:
+#        print(node.val)
+#        node = node.next
