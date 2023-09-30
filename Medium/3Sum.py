4192c16e73a28ddf29f623102699c3d00947f1ee
Jeremy <jdavid.rupprecht@gmail.com>
2023-09-29 19:03:39 -0600

    Complete problem 15 3Sum

--- Medium/3Sum.py
+++ Medium/3Sum.py
@@ -2,18 +2,38 @@
 # https://leetcode.com/problems/3sum/
 
 def threeSum(nums):
-
-    nums.sort() # nlogn
+    # sort the list for the two pointers alg to work
+    nums.sort()
     output = []
     p1 = 0
-    p2 = 1
-    p3 = len(nums) - 1
-
-    for p1 in range(len(nums)):                 #n time --> use dominant term --> nlogn
-        total = nums[p1] + nums[p2] + nums[p3]
-        if total == 0:
-            output.append([nums[p1], nums[p2], nums[p3]])
-
+    for p1 in range(len(nums)):
+        # check for and skip duplicates
+        # put not on the first iteration --> p1 > 0
+        # as the first iteration may include the 
+        # matching combo of numbers --> ex: [[0,0,0]]
+        if p1 > 0 and nums[p1] == nums[p1-1]:
+            continue
+        # Two Pointers alg
+        p2 = p1 + 1
+        p3 = len(nums) - 1
+        while p2 < p3:
+            total = nums[p1] + nums[p2] + nums[p3]
+            if total == 0:
+                output.append([nums[p1], nums[p2], nums[p3]])
+                # if the total is found, move one of the pointers, and skip 
+                # duplicates when doing so --> don't have to check for duplicates
+                # for both pointers --> as once a non-duplicate value is found 
+                # with moving pointer, the "total" value will have changed
+                # and a new 3 number pair will be created anyways, so only moving 
+                # one pointer is sufficient
+                p2 += 1
+                while nums[p2] == nums[p2-1] and p2 < p3:
+                    p2 += 1
+            # conditionally change the total
+            elif total < 0:
+                p2 += 1
+            else:
+                p3 -= 1
     return output
 
 if __name__ == "__main__":
