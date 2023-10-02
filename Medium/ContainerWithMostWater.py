a3a30f69d7117da7157a1011a213c3508235026c
Jeremy <jdavid.rupprecht@gmail.com>
2023-10-01 22:37:22 -0600

    Create file Container With Most Water

---
+++ Medium/ContainerWithMostWater.py
@@ -0,0 +1,10 @@
+# 11 Container With Most Water
+# https://leetcode.com/problems/container-with-most-water/
+def maxArea(height):
+    return 49
+
+
+
+if __name__ == "__main__":
+    height = [1,8,6,2,5,4,8,3,7]
+    print(maxArea(height))
