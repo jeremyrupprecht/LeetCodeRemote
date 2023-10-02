45aa66a04b2dca9a6f6e3b48a9f1a3951cb2a6cd
Jeremy <jdavid.rupprecht@gmail.com>
2023-10-02 01:29:44 -0600

    Complete problem 11 Container With Most Water

--- Medium/ContainerWithMostWater.py
+++ Medium/ContainerWithMostWater.py
@@ -1,10 +1,24 @@
 # 11 Container With Most Water
 # https://leetcode.com/problems/container-with-most-water/
+
 def maxArea(height):
-    return 49
-
-
+    l = 0
+    r = len(height)-1
+    maxArea = 0
+    while l < r:
+        area = (r - l) * min(height[l], height[r])
+        if height[l] < height[r]:   
+            l += 1
+        elif height[l] > height[r]:   
+            r -= 1
+        else: 
+            l += 1
+            while height[l] == height[r] and l < r: 
+                l += 1
+        maxArea = max(maxArea, area)
+    return maxArea
 
 if __name__ == "__main__":
     height = [1,8,6,2,5,4,8,3,7]
+    height1 = [1,1]
     print(maxArea(height))
