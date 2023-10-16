beeaf37439b714117d1f90b1973616574878f352
Jeremy <jdavid.rupprecht@gmail.com>
2023-10-16 16:03:12 -0600

    Create problem file 1343

---
+++ Medium/NumberOfSubArraysOfSizeKAndAvgGreaterThanOrEqualToThreshold.py
@@ -0,0 +1,14 @@
+# 1343 Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold
+# https://leetcode.com/problems/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/
+
+def numOfSubarrays(arr, k, threshold):
+
+
+    return 3
+
+if __name__ == "__main__":
+
+    arr = [2,2,2,2,5,5,5,8]
+    k = 3
+    threshold = 4
+    print(numOfSubarrays(arr, k, threshold))
