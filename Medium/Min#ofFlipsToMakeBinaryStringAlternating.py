adf98d857095cc5f2474f9e665ebf447426a72f4
Jeremy <jdavid.rupprecht@gmail.com>
2023-10-20 19:07:49 -0600

    Add (but not complete yet)problem template 1888. Minimum Number of Flips to Make the Binary String Alternating

---
+++ Medium/Min#ofFlipsToMakeBinaryStringAlternating.py
@@ -0,0 +1,14 @@
+# 1888. Minimum Number of Flips to Make the Binary String Alternating
+# https://leetcode.com/problems/minimum-number-of-flips-to-make-the-binary-string-alternating/
+
+def minFlips(s):
+
+    # This question is messed up, come back to to this one later...
+
+    return 2
+
+
+
+if __name__ =="__main__":
+    s = "111000"
+    print(minFlips(s))
